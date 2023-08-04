3c03886ae21139657a7d73d8d1959b83d2db3a18
soskgo8830 <soskgo@gmail.com>
2023-08-04 23:43:55 +0900

    컴마,정산내역 추가

--- components/StepThree.jsx
+++ components/StepThree.jsx
@@ -22,6 +22,15 @@ function StepThree({ step, setStep, people, expenses }) {
     });
   };
 
+  const calculateTotalExpensesForPerson = (personId) => {
+    return calculatedExpenses.reduce((total, calculatedExpense) => {
+      if (calculatedExpense.selectedPeople.includes(personId)) {
+        return total + calculatedExpense.totalPerPerson;
+      }
+      return total;
+    }, 0);
+  };
+
   const handleCalculate = () => {
     const newCalculatedExpenses = expenses.map((expense) => {
       const selectedPeople = selectedPeopleMap[expense.id] || [];
@@ -30,6 +39,11 @@ function StepThree({ step, setStep, people, expenses }) {
         ...expense,
         selectedPeople,
       };
+
+      const totalPerPerson =
+        calculatedExpense.amount / calculatedExpense.selectedPeople.length;
+
+      calculatedExpense.totalPerPerson = totalPerPerson;
 
       return calculatedExpense;
     });
@@ -43,12 +57,16 @@ function StepThree({ step, setStep, people, expenses }) {
     setSelectedPeopleMap({});
   };
 
+  const addCommasToNumber = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  };
+
   return (
     <div className='p-4'>
       {expenses.map((expense) => (
         <div key={expense.id} className='border p-4 mb-4 rounded'>
           <h3 className='text-lg font-semibold'>
-            정산 내용 : {expense.description}
+            정산 내용: {expense.description}
           </h3>
           <div className='flex flex-wrap mt-2'>
             {people.map((person) => (
@@ -97,11 +115,10 @@ function StepThree({ step, setStep, people, expenses }) {
 
       {showCalculated && (
         <div className='mt-8 border p-4 mb-4 rounded'>
-          <h2 className='text-xl font-semibold mb-4'>정산 내역</h2>
           {calculatedExpenses.map((calculatedExpense) => (
             <div key={calculatedExpense.id} className='mb-4'>
               <p>{calculatedExpense.description}</p>
-              <p>총 금액: {calculatedExpense.amount}</p>
+              <p>총 금액: {addCommasToNumber(calculatedExpense.amount)}</p>
               <p>
                 선택된 인원:{' '}
                 {calculatedExpense.selectedPeople
@@ -112,11 +129,21 @@ function StepThree({ step, setStep, people, expenses }) {
                   .join(', ')}
               </p>
               <p>
-                정산 금액:{' '}
-                {(
-                  calculatedExpense.amount /
-                  calculatedExpense.selectedPeople.length
-                ).toFixed(0)}
+                개인 정산 금액:{' '}
+                {addCommasToNumber(calculatedExpense.totalPerPerson.toFixed(0))}
+              </p>
+            </div>
+          ))}
+
+          <hr />
+          <h2 className='text-xl font-semibold mb-4 mt-4'>정산 내역</h2>
+          {people.map((person) => (
+            <div key={person.id} className='mb-2'>
+              <p>
+                {person.name} :{' '}
+                {addCommasToNumber(
+                  calculateTotalExpensesForPerson(person.id).toFixed(0)
+                )}
               </p>
             </div>
           ))}
